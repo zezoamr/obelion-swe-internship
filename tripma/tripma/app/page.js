a7cbafaa2a35e24d2c422fcaeb5aa71ba80d353d
ziad amr shalaby <zezoamr@yandex.com>
2024-08-04 22:33:15 +0300

    fixed css for page, navbar,  banner, popup (issue with translating figma such as fill/hug/..) and added divider

--- tripma/tripma/app/page.js
+++ tripma/tripma/app/page.js
@@ -8,9 +8,13 @@ export default function TripmaPage() {
         <p className={styles.title}>It's more than just a trip</p>
         <div className={styles.searchContainer}>
           <input type="text" placeholder="From where?" className={styles.searchInput} />
+          <div className={styles.divider}> </div>
           <input type="text" placeholder="Where to?" className={styles.searchInput} />
-          <input type="date" className={styles.searchInput} />
-          <input type="number" placeholder="1 Adult" className={styles.searchInput} />
+          <div className={styles.divider}> </div>
+          <input type="date" className={styles.TextInput} />
+          <div className={styles.divider}> </div>
+          <input type="number" placeholder="1 Adult" className={styles.TextInput2} />
+          
           <button className={styles.searchButton}>Search</button>
         </div>
       </main>
